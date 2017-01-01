408ea1e9e1fcdb64d2f3a13aeda9be5e78b49783
Pike Dzurny <Pike-Dzurny@users.noreply.github.com>
2016-12-31 18:43:10 -0600

    Cleaned up the code

--- Main/getQuestions.py
+++ Main/getQuestions.py
@@ -1,27 +1,22 @@
 import requests
 import time
-from pprint import pprint
-import json
 
-def getQuestions(maxanswers=0, votemin=-1, votemax=3, *tags):
+
+def get_questions(max_answers=0, vote_min=-1, vote_max=3, *tags):
     questions = []
     if len(tags) == 0:
         print("You have no tags.")
-        
+
     else:
         for tags in tags:
-            result = requests.get("https://api.stackexchange.com/2.2/search?page=1&order=desc&sort=creation&tagged={0}&site=stackoverflow".format(tags))
+            result = requests.get(
+                "https://api.stackexchange.com/2.2/search?page=1&order=desc&sort=creation&tagged={0}&site=stackoverflow".format(
+                    tags))
             data = result.json()
             for items in data['items']:
-                if items['is_answered'] == False and items['answer_count'] <= maxanswers:
-                    if items['score'] >= votemin and items['score'] <= votemax:
-                        questions.append([items['link'], items['title']],)
+                if items['is_answered'] is False and items['answer_count'] <= max_answers:
+                    if items['score'] >= vote_min and items['score'] <= vote_max:
+                        questions.append([items['link'], items['title']], )
             if len(tags) > 1:
                 time.sleep(0.5)
     return questions
-
-
-
-
-    
-
