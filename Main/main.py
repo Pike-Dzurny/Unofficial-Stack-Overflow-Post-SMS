b16c988b3a74c5cd6ca0a010dbc3719e1a80ec13
Pike Dzurny <Pike-Dzurny@users.noreply.github.com>
2017-03-11 19:27:39 -0600

    added tags

--- Main/main.py
+++ Main/main.py
@@ -6,9 +6,10 @@ from sendMessage import send_message
 max_answers = 0
 vote_min = -3
 vote_max = 1
+tags = ['python']
 
 while 1:
-    content = get_questions(max_answers, vote_min, vote_max, 'python')
+    content = get_questions(max_answers, vote_min, vote_max, tags)
     content = random.choice(content)
     time.sleep(3)
     sendMessage(content)
